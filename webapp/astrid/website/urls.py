43c6c0446f0ddb0dc822c65d70b137bbd11bfc91
Joe Forsman <jrforsman@gmail.com>
2020-10-01 22:37:28 -0500

    basic log in system and some basic routes

---
+++ webapp/astrid/website/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path
+
+from django.contrib.auth import views as auth_views
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    
+    path('login/', auth_views.LoginView.as_view(redirect_authenticated_user=True, template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('register/', views.register, name='register'),
+    path('users/<int:user_id>', views.userPage, name='userPage'),
+]
--- webapp/astrid/website/views.py
+++ webapp/astrid/website/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import views as auth_views
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect, HttpResponse
+from django.urls import reverse_lazy
+
+from django import forms
+from .forms import UserRegistrationForm
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html', {})
+    
+def register(request):
+    # ensure that logged in users cannot see this page
+    if request.user != None and request.user.is_authenticated:
+        # TODO: notify the user that he cannot access that page when logged in
+        return HttpResponseRedirect(reverse_lazy('index'))
+
+    if request.method == 'POST':
+        # create and save a new username
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            userObj = form.cleaned_data
+            username = userObj['username']
+            email =  userObj['email']
+            password =  userObj['password']
+
+            if not (User.objects.filter(username=username).exists() or User.objects.filter(email=email).exists()):
+                User.objects.create_user(username, email, password)
+
+                user = authenticate(username = username, password = password)
+
+                login(request, user)
+
+                return HttpResponseRedirect(reverse_lazy('index'))
+            else:
+                raise forms.ValidationError('Looks like that username is already in use')
+    else:
+        # render the page with the form to sign up
+        form = UserRegistrationForm()
+
+    # only hit this if either of the else cases are hit
+    return render(request, 'register.html', {'form' : form})
+
+def userPage(request, user_id):
+    user = get_object_or_404(User, pk=user_id)
+
+    return render(request, 'user.html', {'pageUser': user})
