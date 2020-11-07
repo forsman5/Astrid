12e297c8943f2f9ebd17dab5c7aaa593e9d5521b
Joe Forsman <jrforsman@gmail.com>
2020-11-07 13:55:09 -0600

    oops local testing

--- webapp/astrid/website/motor.py
+++ webapp/astrid/website/motor.py
@@ -1,18 +1,17 @@
-#import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 from time import sleep
 
 def turnMotor(sleepTime = 1):
-    #GPIO.setmode(GPIO.BOARD)
-    #GPIO.setup(12, GPIO.OUT)
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(12, GPIO.OUT)
 
-    #pwm = GPIO.PWM(12, 50)
-    #pwm.start(0)
+    pwm = GPIO.PWM(12, 50)
+    pwm.start(0)
 
-    #pwm.ChangeDutyCycle(5)
-    #sleep(sleepTime)
-    #pwm.ChangeDutyCycle(10)
-    #sleep(sleepTime)
+    pwm.ChangeDutyCycle(5)
+    sleep(sleepTime)
+    pwm.ChangeDutyCycle(10)
+    sleep(sleepTime)
 
-    #pwm.stop()
-    #GPIO.cleanup()
-    pass
+    pwm.stop()
+    GPIO.cleanup()
